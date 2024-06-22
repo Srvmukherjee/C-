c02a15f9846d981d6e9209d38a2d2024ab0c126b
Sourav@2820 <souravmukherjee1111999@gmail.com>
2024-06-22 18:01:17 +0530

    updation

---
+++ lec3Variables..cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a = 12;
+    cout<<sizeof(a)<<"\n";
+
+    float b =2.4242;
+    cout<<sizeof(b)<<"\n";
+
+    double c = 2.22414114141;
+    cout<<sizeof(c)<<"\n";
+
+    char d ;
+    cout<<sizeof(d)<<"\n";
+
+    bool e;
+    cout<<sizeof(e)<<"\n";
+
+}
